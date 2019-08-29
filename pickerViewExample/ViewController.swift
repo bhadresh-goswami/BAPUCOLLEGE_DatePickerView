6df627650ee4b503c8b40f030f6e0e92388b1730
COE-18 <coe-18@expert-imac.local>
2019-08-29 16:00:33 +0530

    uploaded

--- pickerViewExample/ViewController.swift
+++ pickerViewExample/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblDate: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func SelectDateAction(_ sender: UIDatePicker) {
+        
+        //lblDate.text = "\(sender.date)"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM, d (h:m:s)"
+        lblDate.text = dateFormatter.string(from: sender.date)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
